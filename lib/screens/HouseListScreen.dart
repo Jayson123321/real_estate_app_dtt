d1acb9269308ab74c6b517fecbd814f17e0be4d0
Jayson <jayson.haverkamp@hva.nl>
2025-04-29 21:37:42 +0200

    - Created _HouseListViewState for HouseListScreen to be stateful - Added Future<List<RealEstateModel>> futureHouses and initState to fetch houses - Added BottomNavigationBar with home and info icons to HouseListScreen

--- lib/screens/HouseListScreen.dart
+++ lib/screens/HouseListScreen.dart
@@ -1,15 +1,44 @@
 import 'package:flutter/material.dart';
 
-class HouseListScreen extends StatelessWidget {
+import '../models/RealEstateModel.dart';
+import '../services/RealEstateService.dart';
+
+class HouseListScreen extends StatefulWidget {
+  @override
+  State<HouseListScreen> createState() => _HouseListViewState();
+}
+
+class _HouseListViewState extends State<HouseListScreen> {
+  late Future<List<RealEstateModel>> futureHouses;
+
+  @override
+  void initState() {
+    super.initState();
+    futureHouses = RealEstateService().fetchHouses();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('House List'),
-    ),
-    body: Center(
-      child: Text('This is the house list view'),
-    ),
+        title: Text('Houses'),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: '',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.info),
+            label: '',
+          ),
+        ],
+        currentIndex: 0,
+        onTap: (index) {
+          // Handle bottom navigation
+        },
+      ),
     );
   }
 }
